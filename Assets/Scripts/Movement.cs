50ee11af4698eaa21c7ccc655f87c5919f1ad051
Tejas <sawanttejasar@gmail.com>
2023-06-22 16:34:02 +0530

    Used input binding concept to rotate and fly the rocket using keys

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,6 +13,21 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
     }
+    void ProcessThrust(){
+        if (Input.GetKey(KeyCode.Space)){
+            Debug.Log("Pressed Space - Thrustling");
+        }
+    }
+    void ProcessRotation(){
+        if (Input.GetKey(KeyCode.A)){
+            Debug.Log("Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            Debug.Log("Rotate Right");
+        }
+    }
+
 }
