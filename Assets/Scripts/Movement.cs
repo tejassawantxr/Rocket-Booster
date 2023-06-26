3a09a16b88e9f727da4fbab4e7f22abad986c848
Tejas <sawanttejasar@gmail.com>
2023-06-26 21:48:52 +0530

    configured to play  thrusting sound when space key is pressed and stopping the audio when we release space key

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
     [SerializeField] float mainThrust = 10f; 
     [SerializeField] float rotationThrust = 1f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); 
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,13 @@ public class Movement : MonoBehaviour
     void ProcessThrust(){
         if (Input.GetKey(KeyCode.Space)){
            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+           if(!audioSource.isPlaying)
+           {
+                audioSource.Play();
+           }
+        }
+        else{
+           audioSource.Stop();
         }
     }
     void ProcessRotation(){
