ef522aa7e841120671d1f1f6e6500dd70729cce0
Tejas <sawanttejasar@gmail.com>
2023-06-27 22:05:11 +0530

    Restart the level, when Rocket crashes to obstacle or ground

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+  void OnCollisionEnter(Collision other) {
+    switch(other.gameObject.tag)
+    {
+        case "Friendly":
+        Debug.Log("This thing is friendly");
+        break;
+
+        case "Finish":
+        Debug.Log("Congrats, you finished the level");
+        break;
+
+        case "Fuel":
+        Debug.Log("You picked up fuel");
+        break;
+
+        default:
+        ReloadLevel();
+        break;
+    }
+  }
+
+  void ReloadLevel(){
+    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    SceneManager.LoadScene(currentSceneIndex);
+  }
+}
