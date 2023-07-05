f803c5c1d91494b05a119623f78ae0b70e8b2a57
Tejas <sawanttejasar@gmail.com>
2023-07-05 15:53:06 +0530

    script to toggle booster on thrust

---
+++ Assets/Scripts/FlameToggle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameToggle : MonoBehaviour
+{
+    Renderer meshrenderer;
+    void Start()
+    {
+        meshrenderer = GetComponent<SkinnedMeshRenderer>();
+        meshrenderer.enabled=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)){
+           meshrenderer.enabled = true;
+        }else{
+            meshrenderer.enabled = false;
+        }
+    }
+}
